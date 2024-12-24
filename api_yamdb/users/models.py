d1bd82875fdd459293c404f31bff1cfbac7db464
Стефан Максименко <cnefan@mail.ru>
2024-12-24 11:24:30 +0300

    chore: add ruff config

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 
 ROLE_CHOICES = (
-    ("user", "user"),
-    ("moderator", "moderator"),
-    ("admin", "admin"),
+    ('user', 'user'),
+    ('moderator', 'moderator'),
+    ('admin', 'admin'),
 )
+
 
 class YamdbUser(AbstractUser):
     bio = models.TextField('Биография', blank=True)
-    role = models.CharField('Роль', default='user', max_length=9, choices=ROLE_CHOICES)
+    role = models.CharField(
+        'Роль', default='user', max_length=9, choices=ROLE_CHOICES
+    )
