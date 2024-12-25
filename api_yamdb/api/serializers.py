445ae9b8dc750ab914666fd9ee4438101031df50
Vladimir Korolev <vovsnmail@gmail.com>
2024-12-25 16:55:22 +0300

    serializers for review and comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category, Genres, Titles
+from reviews.models import Category, Genre, Title, Review, Comment
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -14,7 +14,7 @@ class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
         exclude = ['id']
-        model = Genres
+        model = Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
@@ -23,4 +23,31 @@ class TitleSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = '__all__'
-        model = Titles
+        model = Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        exclude = ('title',)
+
+    def validate_score(self, value):
+        if not (1 <= value <= 10):
+            raise serializers.ValidationError('Рейтинг должен быть от 1 до 10')
+        return value
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        exclude = ('review',)
